54574fb1ce085ae770a5ba1f822eb1c8c042528d
Sat0me <97870659+Sat0me@users.noreply.github.com>
2024-02-06 09:42:14 +0400

    Add files via upload

---
+++ opcserver/opcserver/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace opcserver
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Object<T>
+        {
+            public static string Accessibility;
+            public static T Value;
+
+
+            public Object(string Acc, T Val)
+            {
+                Accessibility = Acc;
+                Value = Val;
+            }
+            public void print()
+            {
+                MessageBox.Show(Value.ToString());
+            }
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            var d = new Object<string>("RW", "4");
+            d.print();
+        }
+
+    }
+}
