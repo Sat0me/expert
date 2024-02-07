40a53e8b0f3a5878ba9c94735b268711308ec3ab
Sat0me <97870659+Sat0me@users.noreply.github.com>
2024-02-07 14:45:45 +0400

    Add files via upload

--- ConsoleApp9/ConsoleApp9/Program.cs
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -1,18 +1,12 @@
-﻿using Nancy.Json;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
-using Newtonsoft.Json.Linq;
-using System.Xml.Linq;
+using System.Diagnostics;
 
-string json = "json1.json";
-string clickgroup = "Group2";
-
-async Task JsonAdd(string json, string clickgroup)
+async Task JsonAdd(string json, string clickgroup, Tag tom)
 {
     //Читаем json
     Rootobject root = new Rootobject();
@@ -23,23 +17,26 @@ async Task JsonAdd(string json, string clickgroup)
     //Добавляем тег
     using (FileStream fs = new FileStream("json1.json", FileMode.Create))
     {
-        Tag tom = new Tag("Tag3", "string", false, "W", "abc");
         Group mix = new Group(tom);
         Server server = new Server(new List<Group> { mix });
         int count = 0;
+        bool tagexist = false; 
         foreach (var i in root.Server)
         {
-            if (i.NameGroup == clickgroup) { root.Server[count].Group.Add(mix); break; }
+            foreach(var j in root.Server[count].Group)
+            {
+                if (j.Tag.Name == tom.Name) { tagexist = true; Console.WriteLine("Такой тег уже существует"); }
+            }
+            if (tagexist == false && i.NameGroup == clickgroup) { root.Server[count].Group.Add(mix); break; }
             count++;
         }
-        server.NameGroup = clickgroup;
 
         var options = new JsonSerializerOptions { WriteIndented = true };
         await System.Text.Json.JsonSerializer.SerializeAsync<Rootobject>(fs, root, options);
     }
 }
 
-async Task JsonDelete(string json, string clickgroup)
+async Task JsonDelete(string json, string clickgroup, int indextag)
 {
     //Читаем json
     Rootobject root = new Rootobject();
@@ -50,28 +47,68 @@ async Task JsonDelete(string json, string clickgroup)
     //Удаляем тег
     using (FileStream fs = new FileStream("json1.json", FileMode.Create))
     {
-        Tag tom = new Tag("Tag3", "string", false, "W", "abc");
-        Group mix = new Group(tom);
-        Server server = new Server(new List<Group> { mix });
         int count = 0;
         foreach (var i in root.Server)
         {
-            if (i.NameGroup == clickgroup) { root.Server[count].Group.RemoveAt(2); break; }
+            if (indextag >= root.Server[count].Group.Count) { Console.WriteLine("Такого тега не существует"); break; }
+            if (i.NameGroup == clickgroup) { root.Server[count].Group.RemoveAt(indextag); break; }
             count++;
         }
-        server.NameGroup = clickgroup;
 
         var options = new JsonSerializerOptions { WriteIndented = true };
         await System.Text.Json.JsonSerializer.SerializeAsync<Rootobject>(fs, root, options);
     }
 }
 
+async Task JsonEdit(string json, string clickgroup, int indextag, Tag tom)
+{
+    //Читаем json
+    Rootobject root = new Rootobject();
+    using (FileStream fs = new FileStream("json1.json", FileMode.OpenOrCreate))
+    {
+        root = await System.Text.Json.JsonSerializer.DeserializeAsync<Rootobject>(fs);
+    }
+    //Удаляем тег и добавляем новый
+    using (FileStream fs = new FileStream("json1.json", FileMode.Create))
+    {
+        Group mix = new Group(tom);
+        Server server = new Server(new List<Group> { mix });
+        int count = 0;
+        bool tagexist = false;
+        foreach (var i in root.Server)
+        {
+            if (indextag >= root.Server[count].Group.Count) { Console.WriteLine("Такого тега не существует"); break; }
+            foreach (var j in root.Server[count].Group)
+            {
+                if (j.Tag.Name == tom.Name) { tagexist = true; Console.WriteLine("Такой тег уже существует"); }
+            }
+            if (tagexist == false && i.NameGroup == clickgroup) { root.Server[count].Group.RemoveAt(indextag); root.Server[count].Group.Add(mix); break; }
+            count++;
+        }
 
-//await JsonAdd(json, clickgroup);
-await JsonDelete(json, clickgroup);
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        await System.Text.Json.JsonSerializer.SerializeAsync<Rootobject>(fs, root, options);
+    }
+}
 
+//main
+string json = "json1.json";
+string clickgroup = "Group2";
+Tag tom = new Tag("Tag4", "String", false, "W", "abc");
 
+//Проверка на соответсвие типа данных с указанным в json
+Type type1 = tom.Value.GetType();
+if (type1.Name == tom.Type)
+{
+    //функции добавления, удаления и редактирования тегов
+    await JsonAdd(json, clickgroup, tom);
+    //await JsonDelete(json, clickgroup, 2);
+    //await JsonEdit(json, clickgroup, 2, tom);
+}
+else Console.WriteLine("Тип данных не соответсвует значению");
 
+//Типы данных, которые можно использовать
+List<string> types = new List<string>() { "Int32", "Int64", "Char", "Boolean", "String" };
 
 public class Rootobject
 {
@@ -112,6 +149,14 @@ public class Tag
     public bool Work { get; set; }
     public string Access { get; set; }
     public dynamic Value { get; set; }
+    public Tag()
+    {
+        Name = "";
+        Type = "String";
+        Work = true;
+        Access = "RW";
+        Value = "";
+    }
     public Tag(string name, string type, bool work, string access, dynamic value)
     {
         Name = name;
